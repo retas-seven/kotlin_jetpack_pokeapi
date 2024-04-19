3d0456a278f137100c0077c6b7d7006d81f156b5
t-osawa <retas_seven@hotmail.co.jp>
2024-04-19 09:54:32 +0900

    テストコミット

--- app/src/main/java/com/example/app/presentation/ScreenRoute.kt
+++ app/src/main/java/com/example/app/presentation/ScreenRoute.kt
@@ -1,9 +1,9 @@
 package com.example.app.presentation
 
 sealed class ScreenRoute(val route: String) {
+    object MyTest : ScreenRoute("mytest_screen")
     object PokemonSearchScreen : ScreenRoute("pokemon_search_screen")
     object PokemonListScreen : ScreenRoute("pokemon_list_screen")
     object PokemonDetailScreen : ScreenRoute("pokemon_detail_screen")
-
 }
 
