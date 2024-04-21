f3f77fe0da6e333b9c0834e3892a161a20114118
t-osawa <retas_seven@hotmail.co.jp>
2024-04-21 17:19:48 +0900

    mytest 修正

--- app/src/main/java/com/example/app/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/app/presentation/ui/theme/Type.kt
@@ -13,8 +13,8 @@ import com.example.app.R
  */
 val Kaisei = FontFamily(
     Font(R.font.kaiseiopti_regular, FontWeight.W400),
-    Font(R.font.kaiseiopti_medium, FontWeight.W500),
-    Font(R.font.kaiseiopti_bold, FontWeight.W700)
+    Font(R.font.kaiseiopti_medium, FontWeight.W600),
+    Font(R.font.kaiseiopti_bold, FontWeight.W800)
 )
 
 val tmp = Typography()
